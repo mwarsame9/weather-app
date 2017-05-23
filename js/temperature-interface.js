41246b14d23e9e62343583d638f35427b4bd1c93
Jin Camou and Mohamed Warsame <mohamed@>
2017-05-23 15:45:59 -0700

    add conditional statement to give user option of selecting celsius or fahrenheit

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -1,15 +1,19 @@
 var Temp = require('./../js/temperature.js').tempModule;
 
-var displayTemp = function(celsius, fahrenheit) {
-  $('.showCelsius').append(`The temp is ${celsius}\u00B0C`);
-  $('.showFahrenheit').append(`The temp is ${fahrenheit}\u00B0F`);
-}
 
 $(document).ready(function() {
   var currentTempObject = new Temp();
   $('#temp-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
+    var tempConvert = $("#temp-convert").val();
+    var displayTemp = function(celsius, fahrenheit) {
+      if (tempConvert == "celsius") {
+        $('.showCelsius').append(`The temp is ${celsius}\u00B0C`);
+      } else {
+        $('.showFahrenheit').append(`The temp is ${fahrenheit}\u00B0F`);
+      }
+    }
     currentTempObject.getTemp(city, displayTemp);
   });
 });
